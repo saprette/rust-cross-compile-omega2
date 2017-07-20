1fcdb84e52a34d5eed16554d005695cbcb95a984
saprette <talaran.goo@gmail.com>
2017-07-20 12:30:11 +0200

    Test web hello world

--- example/hello_web/src/main.rs
+++ example/hello_web/src/main.rs
@@ -1,54 +1,22 @@
-//Include macros to be able to use `insert_routes!`.
-#[macro_use]
-extern crate log;
-extern crate env_logger;
-
-extern crate rustful;
-
-use std::error::Error;
-
-use rustful::{Server, Context, Response, DefaultRouter};
-
-fn say_hello(context: Context, response: Response) {
-    //Get the value of the path variable `:person`, from below.
-    let person = match context.variables.get("person") {
-        Some(name) => name,
-        None => "stranger".into()
-    };
-
-    //Use the name from the path variable to say hello.
-    response.send(format!("Hello, {}!", person));
-}
+extern crate tiny_http;
 
 fn main() {
-    env_logger::init().unwrap();
+    use tiny_http::{Server, Response};
 
-    println!("Visit http://localhost:8080 or http://localhost:8080/Olivia (if your name is Olivia) to try this example.");
+    let bind = "0.0.0.0:8000";
 
-    //Create a DefaultRouter and fill it with handlers.
-    let mut router = DefaultRouter::<fn(Context, Response)>::new();
-    router.build().many(|mut node| {
-        //Handle requests for root...
-        node.then().on_get(say_hello);
+    let server = Server::http(bind).unwrap();
 
-        //...and one level below.
-        //`:person` is a path variable and it will be accessible in the handler.
-        node.path(":person").then().on_get(say_hello);
-    });
+    println!("Server started bind on {}", bind);
 
-    //Build and run the server.
-    let server_result = Server {
-        handlers: router,
+    for request in server.incoming_requests() {
+        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
+                 request.method(),
+                 request.url(),
+                 request.headers()
+        );
 
-        //Turn a port number into an IPV4 host address (0.0.0.0:8080 in this case).
-        host: 8080.into(),
-
-        //Use default values for everything else.
-        ..Server::default()
-    }.run();
-
-    match server_result {
-        Ok(_server) => {},
-        Err(e) => error!("could not start server: {}", e.description())
+        let response = Response::from_string("hello world");
+        request.respond(response);
     }
 }
